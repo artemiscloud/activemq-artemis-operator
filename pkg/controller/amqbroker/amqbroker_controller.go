98517aaf3db2ab8f84ddf01c0433553fa4703179
Roddie Kieley <rkieley@apache.org>
2019-03-13 10:49:19 -0230

    ENTMQBR-2171: Added StatefulSet and headless service with default ports for internal to OCP SDN use.

--- pkg/controller/amqbroker/amqbroker_controller.go
+++ pkg/controller/amqbroker/amqbroker_controller.go
@@ -21,6 +21,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	appsv1 "k8s.io/api/apps/v1"
 )
 
 var log = logf.Log.WithName("controller_amqbroker")
@@ -125,24 +127,6 @@ func (r *ReconcileAMQBroker) Reconcile(request reconcile.Request) (reconcile.Res
 			break
 		}
 
-		// Pod didn't exist, create
-		// Define a new Pod object
-		pod := newPodForCR(instance)
-		// Set AMQBroker instance as the owner and controller
-		if err = controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
-			// Add error detail for use later
-			break
-		}
-		// Was able to set the owner and controller, call k8s create for pod
-		if err = r.client.Create(context.TODO(), pod); err != nil {
-			// Add error detail for use later
-			break
-		}
-
-		// Pod created successfully - don't requeue
-		// Probably redundant
-		err = nil
-
 		// Define the console-jolokia Service for this Pod
 		consoleJolokiaSvc := newServiceForCR(instance, "console-jolokia", 8161)
 		// Set AMQBroker instance as the owner and controller
@@ -169,6 +153,55 @@ func (r *ReconcileAMQBroker) Reconcile(request reconcile.Request) (reconcile.Res
 			break
 		}
 
+		// Define the headless Service for the StatefulSet
+		//headlessSvc := newHeadlessServiceForCR(instance, 5672)
+		headlessSvc := newHeadlessServiceForCR(instance, getDefaultPorts())
+		// Set AMQBroker instance as the owner and controller
+		if err = controllerutil.SetControllerReference(instance, headlessSvc, r.scheme); err != nil {
+			// Add error detail for use later
+			break
+		}
+		// Call k8s create for service
+		if err = r.client.Create(context.TODO(), headlessSvc); err != nil {
+			// Add error detail for use later
+			break
+		}
+
+
+		//// Pod didn't exist, create
+		//// Define a new Pod object
+		//pod := newPodForCR(instance)
+		//// Set AMQBroker instance as the owner and controller
+		//if err = controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
+		//	// Add error detail for use later
+		//	break
+		//}
+		//// Was able to set the owner and controller, call k8s create for pod
+		//if err = r.client.Create(context.TODO(), pod); err != nil {
+		//	// Add error detail for use later
+		//	break
+		//}
+
+		// Statefulset didn't exist, create
+		// Define a new Pod object
+		ss := newStatefulSetForCR(instance)
+		// Set AMQBroker instance as the owner and controller
+		if err = controllerutil.SetControllerReference(instance, ss, r.scheme); err != nil {
+			// Add error detail for use later
+			break
+		}
+		// Was able to set the owner and controller, call k8s create for pod
+		if err = r.client.Create(context.TODO(), ss); err != nil {
+			// Add error detail for use later
+			break
+		}
+
+
+		// Pod created successfully - don't requeue
+		// Probably redundant
+		err = nil
+
+
 		break
 	}
 
@@ -187,6 +220,93 @@ func (r *ReconcileAMQBroker) Reconcile(request reconcile.Request) (reconcile.Res
 
 	// Single exit, return the result and error condition
 	return reconcileResult, err
+}
+
+func newStatefulSetForCR(cr *brokerv1alpha1.AMQBroker) *appsv1.StatefulSet {
+
+	// Log where we are and what we're doing
+	reqLogger:= log.WithName(cr.Name)
+	reqLogger.Info("Creating new statefulset for custom resource")
+
+	var replicas int32 = 1
+
+	labels := selectors.LabelsForAMQBroker(cr.Name)
+
+	ss := &appsv1.StatefulSet{
+		TypeMeta: metav1.TypeMeta{
+			Kind:		"StatefulSet",
+			APIVersion:	"v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:			cr.Name + "-statefulset",
+			Namespace:		cr.Namespace,
+			Labels:			labels,
+			Annotations:	nil,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: 		&replicas,
+			ServiceName:	cr.Name + "-headless" + "-service",
+			Selector:		&metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			//Template: corev1.PodTemplateSpec{},
+			Template: newPodTemplateSpecForCR(cr),
+		},
+	}
+
+	return ss
+}
+
+func newPodTemplateSpecForCR(cr *brokerv1alpha1.AMQBroker) corev1.PodTemplateSpec {
+
+	// Log where we are and what we're doing
+	reqLogger:= log.WithName(cr.Name)
+	reqLogger.Info("Creating new pod template spec for custom resource")
+
+	//var pts corev1.PodTemplateSpec
+
+	dataPath := "/opt/" + cr.Name + "/data"
+	userEnvVar := corev1.EnvVar{"AMQ_USER", "admin", nil}
+	passwordEnvVar := corev1.EnvVar{"AMQ_PASSWORD", "admin", nil}
+	dataPathEnvVar := corev1.EnvVar{ "AMQ_DATA_DIR", dataPath, nil}
+
+	pts := corev1.PodTemplateSpec{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name,
+			Namespace: cr.Namespace,
+			Labels:    cr.Labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    cr.Name + "-container",
+					Image:	 cr.Spec.Image,
+					Command: []string{"/opt/amq/bin/launch.sh", "start"},
+					VolumeMounts:	[]corev1.VolumeMount{
+						corev1.VolumeMount{
+							Name:		cr.Name + "-data-volume",
+							MountPath:	dataPath,
+							ReadOnly:	false,
+						},
+					},
+					Env:     []corev1.EnvVar{userEnvVar, passwordEnvVar, dataPathEnvVar},
+				},
+			},
+			Volumes: []corev1.Volume{
+				corev1.Volume{
+					Name: cr.Name + "-data-volume",
+					VolumeSource: corev1.VolumeSource{
+						PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+							ClaimName: cr.Name + "-pvc",
+							ReadOnly:  false,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return pts
 }
 
 func newPersistentVolumeClaimForCR(cr *brokerv1alpha1.AMQBroker) *corev1.PersistentVolumeClaim {
@@ -260,6 +380,85 @@ func newServiceForCR(cr *brokerv1alpha1.AMQBroker, name_suffix string, port_numb
 	return svc
 }
 
+func getDefaultPorts() *[]corev1.ServicePort {
+
+	ports := []corev1.ServicePort{
+		corev1.ServicePort{
+			Name:		"mqtt",
+			Protocol:	"TCP",
+			Port:		1883,
+			TargetPort:	intstr.FromInt(int(1883)),
+		},
+		corev1.ServicePort{
+			Name:		"amqp",
+			Protocol:	"TCP",
+			Port:		5672,
+			TargetPort:	intstr.FromInt(int(5672)),
+		},
+		corev1.ServicePort{
+			Name:		"console-jolokia",
+			Protocol:	"TCP",
+			Port:		8161,
+			TargetPort:	intstr.FromInt(int(8161)),
+		},
+		corev1.ServicePort{
+			Name:		"stomp",
+			Protocol:	"TCP",
+			Port:		61613,
+			TargetPort:	intstr.FromInt(int(61613)),
+		},
+		corev1.ServicePort{
+			Name:		"all",
+			Protocol:	"TCP",
+			Port:		61616,
+			TargetPort:	intstr.FromInt(int(61616)),
+		},
+	}
+
+	return &ports
+}
+
+// newServiceForPod returns an amqbroker service for the pod just created
+func newHeadlessServiceForCR(cr *brokerv1alpha1.AMQBroker, servicePorts *[]corev1.ServicePort) *corev1.Service {
+
+	// Log where we are and what we're doing
+	reqLogger := log.WithValues("AMQBroker Name", cr.Name)
+	reqLogger.Info("Creating new " + "headless" + " service")
+
+	labels := selectors.LabelsForAMQBroker(cr.Name)
+
+
+	//port := corev1.ServicePort{
+	//	Name:		cr.Name + "-" + "headless" + "-port",
+	//	Protocol:	"TCP",
+	//	Port:		port_number,
+	//	TargetPort:	intstr.FromInt(int(port_number)),
+	//}
+	//ports := []corev1.ServicePort{}
+	//ports = append(ports, port)
+
+	svc := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:		"Service",
+		},
+		ObjectMeta: metav1.ObjectMeta {
+			Annotations: 	nil,
+			Labels: 		labels,
+			Name:			"headless" + "-service",
+			Namespace:		cr.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: 	"ClusterIP",
+			Ports: 	*servicePorts,
+			Selector: labels,
+			ClusterIP: "None",
+			PublishNotReadyAddresses: true,
+		},
+	}
+
+	return svc
+}
 
 // newPodForCR returns an amqbroker pod with the same name/namespace as the cr
 func newPodForCR(cr *brokerv1alpha1.AMQBroker) *corev1.Pod {
