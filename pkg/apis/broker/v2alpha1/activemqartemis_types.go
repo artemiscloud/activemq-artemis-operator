7a0af94135b6f5784f5a53c28c9a9c1e4b6b7eb4
Mikhail Krutov <mkrutov@redhat.com>
2020-05-13 11:29:14 +0200

    Restore v2alpha1 typedefs to be able to work with v2alpha1 client

---
+++ pkg/apis/broker/v2alpha1/activemqartemis_types.go
@@ -0,0 +1,111 @@
+package v2alpha1
+
+import (
+	"github.com/RHsyseng/operator-utils/pkg/olm"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ActiveMQArtemisSpec defines the desired state of ActiveMQArtemis
+// +k8s:openapi-gen=true
+type ActiveMQArtemisSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	AdminUser      string             `json:"adminUser,omitempty"`
+	AdminPassword  string             `json:"adminPassword,omitempty"`
+	DeploymentPlan DeploymentPlanType `json:"deploymentPlan,omitempty"`
+	Acceptors      []AcceptorType     `json:"acceptors,omitempty"`
+	Connectors     []ConnectorType    `json:"connectors,omitempty"`
+	Console        ConsoleType        `json:"console,omitempty"`
+}
+
+type DeploymentPlanType struct {
+	Image              string `json:"image,omitempty"`
+	Size               int32  `json:"size,omitempty"`
+	RequireLogin       bool   `json:"requireLogin,omitempty"`
+	PersistenceEnabled bool   `json:"persistenceEnabled,omitempty"`
+	JournalType        string `json:"journalType,omitempty"`
+	MessageMigration   *bool  `json:"messageMigration,omitempty"`
+}
+
+type AcceptorType struct {
+	Name                string `json:"name"`
+	Port                int32  `json:"port,omitempty"`
+	Protocols           string `json:"protocols,omitempty"`
+	SSLEnabled          bool   `json:"sslEnabled,omitempty"`
+	SSLSecret           string `json:"sslSecret,omitempty"`
+	EnabledCipherSuites string `json:"enabledCipherSuites,omitempty"`
+	EnabledProtocols    string `json:"enabledProtocols,omitempty"`
+	NeedClientAuth      bool   `json:"needClientAuth,omitempty"`
+	WantClientAuth      bool   `json:"wantClientAuth,omitempty"`
+	VerifyHost          bool   `json:"verifyHost,omitempty"`
+	SSLProvider         string `json:"sslProvider,omitempty"`
+	SNIHost             string `json:"sniHost,omitempty"`
+	Expose              bool   `json:"expose,omitempty"`
+	AnycastPrefix       string `json:"anycastPrefix,omitempty"`
+	MulticastPrefix     string `json:"multicastPrefix,omitempty"`
+	ConnectionsAllowed  int    `json:"connectionsAllowed,omitempty"`
+}
+
+type ConnectorType struct {
+	Name                string `json:"name"`
+	Type                string `json:"type,omitempty"`
+	Host                string `json:"host"`
+	Port                int32  `json:"port"`
+	SSLEnabled          bool   `json:"sslEnabled,omitempty"`
+	SSLSecret           string `json:"sslSecret,omitempty"`
+	EnabledCipherSuites string `json:"enabledCipherSuites,omitempty"`
+	EnabledProtocols    string `json:"enabledProtocols,omitempty"`
+	NeedClientAuth      bool   `json:"needClientAuth,omitempty"`
+	WantClientAuth      bool   `json:"wantClientAuth,omitempty"`
+	VerifyHost          bool   `json:"verifyHost,omitempty"`
+	SSLProvider         string `json:"sslProvider,omitempty"`
+	SNIHost             string `json:"sniHost,omitempty"`
+	Expose              bool   `json:"expose,omitempty"`
+}
+
+type ConsoleType struct {
+	Expose        bool   `json:"expose,omitempty"`
+	SSLEnabled    bool   `json:"sslEnabled,omitempty"`
+	SSLSecret     string `json:"sslSecret,omitempty"`
+	UseClientAuth bool   `json:"useClientAuth,omitempty"`
+}
+
+// ActiveMQArtemisStatus defines the observed state of ActiveMQArtemis
+// +k8s:openapi-gen=true
+type ActiveMQArtemisStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	PodStatus olm.DeploymentStatus `json:"podStatus"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ActiveMQArtemis is the Schema for the activemqartemis API
+// +k8s:openapi-gen=true
+// +genclient
+type ActiveMQArtemis struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ActiveMQArtemisSpec   `json:"spec,omitempty"`
+	Status ActiveMQArtemisStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ActiveMQArtemisList contains a list of ActiveMQArtemis
+type ActiveMQArtemisList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ActiveMQArtemis `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ActiveMQArtemis{}, &ActiveMQArtemisList{})
+}
