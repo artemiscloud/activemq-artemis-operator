f99239e4a2bcc20387b2a5dc122ba27a1574acc5
Howard Gao <howard.gao@gmail.com>
2021-12-28 23:41:28 +0800

    More conversion code

--- api/v2alpha5/activemq_artemis_conversion.go
+++ api/v2alpha5/activemq_artemis_conversion.go
@@ -9,11 +9,18 @@ import (
 var log = logf.Log.WithName("v2alpha5Conversion")
 
 func (r *ActiveMQArtemis) ConvertTo(dst conversion.Hub) error {
-	log.Info("==== trying converting from v2alpha5 to v1beta1")
+	log.Info("====* trying converting from v2alpha5 to v1beta1")
 	target := dst.(*v1beta1.ActiveMQArtemis)
-	log.Info("target got", "dst", target.APIVersion)
+	log.Info("target got", "dst", *target)
 
 	target.ObjectMeta = r.ObjectMeta
+	target.Name = r.Name
+	target.Namespace = r.Namespace
+	target.Spec.DeploymentPlan.Size = r.Spec.DeploymentPlan.Size
+	target.Spec.DeploymentPlan.Image = r.Spec.DeploymentPlan.Image
+	target.Spec.DeploymentPlan.PersistenceEnabled = r.Spec.DeploymentPlan.PersistenceEnabled
+
+	log.Info("**** converted to v1beta1", "target", *target)
 	//Todo: covert all artemis cr data to target
 
 	return nil
