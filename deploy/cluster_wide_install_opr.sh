dfd62b04995d50413dfde71f0af454f0a8c269fb
Domenico Francesco Bruscino <brusdev@apache.org>
2022-10-11 21:44:31 +0200

    [#342] Improve deploy scripts
    Execute deploy scripts from any directory and use OpenShift CLI if available.

--- deploy/cluster_wide_install_opr.sh
+++ deploy/cluster_wide_install_opr.sh
@@ -9,13 +9,19 @@ then
   exit 1
 fi
 
-KUBE=kubectl
+DEPLOY_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-$KUBE create -f ./crds
-$KUBE create -f ./service_account.yaml
-$KUBE create -f ./cluster_role.yaml
-$KUBE create -f ./cluster_role_binding.yaml
-$KUBE create -f ./election_role.yaml
-$KUBE create -f ./election_role_binding.yaml
-$KUBE create -f ./operator_config.yaml
-$KUBE create -f ./operator.yaml
+if oc version; then
+    KUBE_CLI=oc
+else
+    KUBE_CLI=kubectl
+fi
+
+$KUBE_CLI create -f $DEPLOY_PATH/crds
+$KUBE_CLI create -f $DEPLOY_PATH/service_account.yaml
+$KUBE_CLI create -f $DEPLOY_PATH/cluster_role.yaml
+$KUBE_CLI create -f $DEPLOY_PATH/cluster_role_binding.yaml
+$KUBE_CLI create -f $DEPLOY_PATH/election_role.yaml
+$KUBE_CLI create -f $DEPLOY_PATH/election_role_binding.yaml
+$KUBE_CLI create -f $DEPLOY_PATH/operator_config.yaml
+$KUBE_CLI create -f $DEPLOY_PATH/operator.yaml
--- deploy/undeploy_all.sh
+++ deploy/undeploy_all.sh
@@ -2,15 +2,21 @@
 
 echo "Undeploy everything..."
 
-KUBE=kubectl
+DEPLOY_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-$KUBE delete -f ./crds
-$KUBE delete -f ./service_account.yaml
-$KUBE delete -f ./role.yaml
-$KUBE delete -f ./role_binding.yaml
-$KUBE delete -f ./cluster_role.yaml
-$KUBE delete -f ./cluster_role_binding.yaml
-$KUBE delete -f ./election_role.yaml
-$KUBE delete -f ./election_role_binding.yaml
-$KUBE delete -f ./operator_config.yaml
-$KUBE delete -f ./operator.yaml
+if oc version; then
+    KUBE_CLI=oc
+else
+    KUBE_CLI=kubectl
+fi
+
+$KUBE_CLI delete -f $DEPLOY_PATH/crds
+$KUBE_CLI delete -f $DEPLOY_PATH/service_account.yaml
+$KUBE_CLI delete -f $DEPLOY_PATH/role.yaml
+$KUBE_CLI delete -f $DEPLOY_PATH/role_binding.yaml
+$KUBE_CLI delete -f $DEPLOY_PATH/cluster_role.yaml
+$KUBE_CLI delete -f $DEPLOY_PATH/cluster_role_binding.yaml
+$KUBE_CLI delete -f $DEPLOY_PATH/election_role.yaml
+$KUBE_CLI delete -f $DEPLOY_PATH/election_role_binding.yaml
+$KUBE_CLI delete -f $DEPLOY_PATH/operator_config.yaml
+$KUBE_CLI delete -f $DEPLOY_PATH/operator.yaml
