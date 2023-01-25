cb73d423ed5cbead611b283f266d54931963b39f
Domenico Francesco Bruscino <brusdev@apache.org>
2023-01-25 15:37:33 +0100

    Reapply cluster_wide_install_opr.sh improvements after reverting "New deployment structure"

--- deploy/cluster_wide_install_opr.sh
+++ deploy/cluster_wide_install_opr.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo "Deploying cluster-wide operator, dont forget change WATCH_NAMESPACE to empty! and cluser-role-binding subjects namespace"
+echo "Deploying cluster-wide operator"
 
-read -p "Please confirm that you have changed WATCH_NAMESPACE and role-binding to proper values [Y/N]: "
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-  exit 1
+read -p "Enter namespaces to watch (empty for all namespaces): " WATCH_NAMESPACE
+if [ -z ${WATCH_NAMESPACE} ]; then
+  WATCH_NAMESPACE="*"
 fi
 
 DEPLOY_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -20,8 +18,9 @@ fi
 $KUBE_CLI create -f $DEPLOY_PATH/crds
 $KUBE_CLI create -f $DEPLOY_PATH/service_account.yaml
 $KUBE_CLI create -f $DEPLOY_PATH/cluster_role.yaml
-$KUBE_CLI create -f $DEPLOY_PATH/cluster_role_binding.yaml
+SERVICE_ACCOUNT_NS="$(kubectl get -f $DEPLOY_PATH/service_account.yaml -o jsonpath='{.metadata.namespace}')"
+sed "s/namespace:.*/namespace: ${SERVICE_ACCOUNT_NS}/" $DEPLOY_PATH/cluster_role_binding.yaml | kubectl apply -f -
 $KUBE_CLI create -f $DEPLOY_PATH/election_role.yaml
 $KUBE_CLI create -f $DEPLOY_PATH/election_role_binding.yaml
 $KUBE_CLI create -f $DEPLOY_PATH/operator_config.yaml
-$KUBE_CLI create -f $DEPLOY_PATH/operator.yaml
+sed -e "/WATCH_NAMESPACE/,/- name/ { /WATCH_NAMESPACE/b; /valueFrom:/bx; /- name/b; d; :x s/valueFrom:/value: '${WATCH_NAMESPACE}'/}" $DEPLOY_PATH/operator.yaml | kubectl apply -f -
