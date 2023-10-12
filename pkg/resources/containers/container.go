25d5717d7ce882679a8fc179c78569f16558d427
Gary Tully <gary.tully@gmail.com>
2023-10-12 10:24:01 +0100

    [#711] use JKD_JAVA_OPTIONS in place of JAVA_OPTS for operator jvm config

--- pkg/resources/containers/container.go
+++ pkg/resources/containers/container.go
@@ -26,7 +26,7 @@ func MakeContainer(hostingPodSpec *corev1.PodSpec, customResourceName string, im
 	if container == nil {
 		container = &corev1.Container{
 			Name:    name,
-			Command: []string{"/bin/sh", "-c", "export STATEFUL_SET_ORDINAL=${HOSTNAME##*-};exec /opt/amq/bin/launch.sh", "start"},
+			Command: []string{"/bin/bash", "-c", "export STATEFUL_SET_ORDINAL=${HOSTNAME##*-};export JDK_JAVA_OPTIONS=${JDK_JAVA_OPTIONS//\\$\\{STATEFUL_SET_ORDINAL\\}/${HOSTNAME##*-}};exec /opt/amq/bin/launch.sh", "start"},
 		}
 	}
 
