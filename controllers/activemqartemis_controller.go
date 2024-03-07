552b40f700e8d2d7664af4c132a6cd1b0e6e5a9d
Liam Shanley <Liam.Shanley@fmr.com>
2024-03-07 12:58:26 +0000

    adding functionality to block reconciliation via an annotation on the CR

--- controllers/activemqartemis_controller.go
+++ controllers/activemqartemis_controller.go
@@ -157,6 +157,14 @@ func (r *ActiveMQArtemisReconciler) Reconcile(ctx context.Context, request ctrl.
 		return result, err
 	}
 
+	        reqLogger.Info("ActiveMQArtemis resource retrieved")
+
+        // Check for the annotation. If it exists, return without reconciling.
+        if val, ok := customResource.Annotations["artemis.broker.amq.io/block-reconcile"]; ok && val == "true" {
+                reqLogger.Info("Reconcile blocked by annotation")
+                return result, nil
+        }
+
 	namer := MakeNamers(customResource)
 	reconciler := NewActiveMQArtemisReconcilerImpl(customResource, r.log, r.Scheme)
 
